1f5a21e51a620733e675892b42d2693bdf852cac
靳鸿鑫 <22580714@qq.com>
2017-11-01 14:52:06 +0800

    init

--- common/services/Constants.php
+++ common/services/Constants.php
@@ -6,4 +6,9 @@ class Constants
 {
     const CODE_SUCCESS = 0;
     const CODE_FAILED = -1;
+
+    const WEB_SOCKET_IP = '127.0.0.1';
+    const WEB_SOCKET_PORT = '9502';
+    const MESSAGE_TYPE_BARRAGE_REQ = 'barrage_req';
+
 }
