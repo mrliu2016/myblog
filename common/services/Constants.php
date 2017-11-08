e36f75ec69caf27e36e56f0b3e3d438fa016ab12
靳鸿鑫 <22580714@qq.com>
2017-11-08 14:58:05 +0800

    消息管理init

--- common/services/Constants.php
+++ common/services/Constants.php
@@ -77,10 +77,11 @@ class Constants
     const ORDER_STATUS_APPROVAL_APPROVAL_ING = 'APPROVAL_ING'; // 审核中
     const ORDER_STATUS_APPROVAL_THROUGH = 'THROUGH'; // 审核通过
     const ORDER_STATUS_APPROVAL_REFUSE = 'REFUSE'; // 审核不通过
-
     const EARTH_RADIUS = 6371; //地球半径，平均半径为6371km
-
     const NUM_WS_ROOM_USER = 100;//每个房间最多保存100个用户的信息
-
     const WS_NOTICE = '文明用语';
+    const WS_MESSAGE_TYPE_WARNING = 0;  //ws 警告
+    const WS_MESSAGE_TYPE_CLOSE = 1;  //ws 强制关闭
+    const WS_KEY_WARNING = 'WSWarning';
+    const WS_KEY_CLOSE = 'WSClose';
 }
--- manager/views/message/index.php
+++ manager/views/message/index.php
@@ -3,4 +3,167 @@ use yii\widgets\LinkPager;
 
 $this->title = '消息管理';
 ?>
-消息管理
+
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-body">
+            <form method="post" action="/message/index" class="form-horizontal" id="searchForm"
+                  name="searchForm">
+                <fieldset style="height: 20px">
+                    <div class="form-group">
+                        <div class="col-sm-10">
+                            <div class="col-md-3">
+                                用户Id： <input type="text" style="width: auto" id="content" name="userId">
+                            </div>
+                            <div class="col-md-2">
+                                <select style="width: auto" name="type" title="" class="form-control">
+                                    <option value="0">警告</option>
+                                    <option value="1">强制退出</option>
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                原因： <input type="text" style="width: auto" id="message" name="message">
+                            </div>
+                            <div class="col-md-1">
+                                <button type="button" class="mb-sm btn btn-primary ripple" id="searchBtn"
+                                        name="searchBtn">发送
+                                </button>
+                            </div>
+                        </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+    <div class="card">
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th class="col-md-1">id</th>
+                    <th class="col-md-1">用户id</th>
+                    <th class="col-md-1">类型</th>
+                    <th class="col-md-1">内容</th>
+                    <th class="col-md-1">创建时间</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($itemList as $item): ?>
+                    <tr>
+                        <td>
+                            <?= $item['id'] ?>
+                        </td>
+                        <td>
+                            <?= $item['userId'] ?>
+                        </td>
+                        <td>
+                            <?= $item['type'] ?>
+                        </td>
+                        <td>
+                            <?= $item['message'] ?>
+                        </td>
+                        <td>
+                            <?= date('Y-m-d H:i:s', $item['created']) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <nav class="text-center">
+        <table>
+            <tr>
+                <td> <?= LinkPager::widget(['pagination' => $pagination]) ?></td>
+                <td>共<?= $count ?> 条</td>
+            </tr>
+        </table>
+    </nav>
+
+</div>
+
+
+<script type="text/javascript">
+
+    $("#searchBtn").click(function () {
+        $("#searchForm").submit()
+    });
+    $("#cleanBtn").click(function () {
+        $(this).closest('form').find("input[type=text]").val("")
+    });
+
+    $(".datepicker-pop").datetimepicker({
+        todayHighlight: true,
+        todayBtn: true,
+        autoclose: true,
+        minView: 3,
+        format: 'yyyy-mm-dd',
+        language: 'zh-CN'
+    });
+
+    function detail(serverName, begin, end) {
+        $.ajax({
+            url: "/report/lost-detail?serverName=" + serverName + '&beginTime=' + begin + '&endTime=' + end,
+            type: "get",
+            cache: false,
+            dataType: "text",
+            success: function (data) {
+                html = '<div id="main_1" style="width:1200px;height:450px;float: left"></div>'
+                layer.open({
+                    title: serverName,
+                    type: 1,
+                    skin: 'layui-layer-rim', //加上边框
+                    area: ['1000px', '550px'], //宽高
+                    content: html
+                });
+
+                var client_array = data;
+                var client_js = eval('(' + client_array + ')');
+                // 初始化图表标签
+                var myChart = echarts.init(document.getElementById('main_1'));
+                options = {
+                    /* title : {
+                     text: '丢包率',
+                     x:'center'
+                     },*/
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        left: 'left',
+                        data: ['0~5%', '5%~10%', '10%~20%', '>20%', '成功']
+                    },
+                    series: [
+                        {
+                            name: '丢包率',
+                            type: 'pie',
+                            radius: '55%',
+                            center: ['50%', '60%'],
+                            data: [
+                                {value: client_js['a'], name: '0~5%'},
+                                {value: client_js['b'], name: '5%~10%'},
+                                {value: client_js['c'], name: '10%~20%'},
+                                {value: client_js['d'], name: '>20%'},
+                                {value: client_js['e'], name: '成功'}
+                            ],
+                            itemStyle: {
+                                normal: {
+                                    label: {
+                                        show: true,
+                                        formatter: '{b} : {c} ({d}%)'
+                                    },
+                                    labelLine: {show: true}
+                                }
+                            }
+                        }
+                    ]
+                };
+                myChart.setOption(options);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert('get issue');
+            }
+        });
+    }
+</script>
