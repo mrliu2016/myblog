2bed926b271138921bc8afcc596b55ece5ebdf76
huangmengrong <huangmengrong@huangmengrong.com>
2018-05-08 11:28:14 +0800

    菜单调整

--- manager/config/menu.php
+++ manager/config/menu.php
@@ -6,9 +6,21 @@
  */
 return [
     [
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '设置', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level2' => [
+            ['permissionId' => '/application/index', 'name' => '设置', 'href' => '/user/index'],
+        ],
+    ],
+    [
         'level1' => ['permissionId' => 'all-selection-manage', 'name' => '用户管理', 'icon' => 'navicon.svg', 'href' => '#'],
         'level2' => [
             ['permissionId' => '/application/index', 'name' => '用户', 'href' => '/user/index'],
+        ],
+    ],
+    [
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '等级管理', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level2' => [
+            ['permissionId' => '/application/index', 'name' => '等级', 'href' => '/user/index'],
         ],
     ],
     [
@@ -20,17 +32,41 @@ return [
         ],
     ],
     [
-        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '充值&提现', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '财务管理', 'icon' => 'navicon.svg', 'href' => '#'],
         'level2' => [
             ['permissionId' => '/application/index', 'name' => '充值历史', 'href' => '/deposit/index'],
             ['permissionId' => '/application/index', 'name' => '提现审核', 'href' => '/deposit/withdraw'],
         ],
     ],
     [
-        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '礼物', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '靓号管理', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level2' => [
+            ['permissionId' => '/application/index', 'name' => '靓号列表', 'href' => '/deposit/index'],
+        ],
+    ],
+    [
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '礼物管理', 'icon' => 'navicon.svg', 'href' => '#'],
         'level2' => [
             ['permissionId' => '/application/index', 'name' => '模版', 'href' => '/gift/template'],
             ['permissionId' => '/application/index', 'name' => '赠送', 'href' => '/gift/order'],
+        ],
+    ],
+    [
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '扩展工具', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level2' => [
+            ['permissionId' => '/application/index', 'name' => '备份管理', 'href' => '/message/index'],
+            ['permissionId' => '/application/index', 'name' => '文件存储', 'href' => '/message/key'],
+            ['permissionId' => '/application/index', 'name' => '后台菜单', 'href' => '/message/key'],
+            ['permissionId' => '/application/index', 'name' => '幻灯片', 'href' => '/message/key']
+        ],
+    ],
+    [
+        'level1' => ['permissionId' => 'all-selection-manage', 'name' => '内容管理', 'icon' => 'navicon.svg', 'href' => '#'],
+        'level2' => [
+            ['permissionId' => '/application/index', 'name' => '用户反馈', 'href' => '/message/index'],
+            ['permissionId' => '/application/index', 'name' => '页面管理', 'href' => '/message/key'],
+            ['permissionId' => '/application/index', 'name' => '分类管理', 'href' => '/message/key'],
+            ['permissionId' => '/application/index', 'name' => '文章管理', 'href' => '/message/key']
         ],
     ],
     [
