9795ded14995ffe36ee580828891db26c7829b0e
huangmengrong <huangmengrong@huangmengrong.com>
2018-05-24 18:36:16 +0800

    礼物连发属性

--- api/controllers/GiftController.php
+++ api/controllers/GiftController.php
@@ -2,6 +2,7 @@
 
 namespace app\api\controllers;
 
+use app\common\models\FusilladeAttribute;
 use app\common\services\Constants;
 use app\common\services\GiftService;
 use Yii;
@@ -33,6 +34,19 @@ class GiftController extends BaseController
     public function actionFusilladeAttribute()
     {
         $params = Yii::$app->request->get();
+        $params['defaultPageSize'] = $size = intval(!empty($params['size']) ? $params['size'] : self::PAGE_SIZE);
+        $page = intval(!empty($params['page']) ? $params['page'] : 0);
+        $list = FusilladeAttribute::queryInfo($params);
+        $totalCount = intval(FusilladeAttribute::queryInfoNum($params));
+        $pageCount = ceil($totalCount / $params['size']);
+        if (!empty($list)) {
+            $this->jsonReturnSuccess(
+                Constants::CODE_SUCCESS,
+                '',
+                compact('totalCount', 'page', 'size', 'pageCount', 'list')
+            );
+        }
+        $this->jsonReturnError(Constants::CODE_FAILED);
     }
 
     /**
