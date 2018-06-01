d35ea46e1d422ca858f73a4fa51c5936cf16d6d7
liuhongzhen <liuhongzhen@3ttech.cn>
2018-06-01 19:16:02 +0800

    直播后台功能

--- manager/views/robot/batch-add.php
+++ manager/views/robot/batch-add.php
@@ -1,17 +1,16 @@
 <?php
 $this->title = '上传文件';
-
 ?>
 <div class="container-fluid">
     <div class="card">
         <div class="card-body">
-            <form method="post" action="/rebot/batch-add" class="form-horizontal" id="searchForm"
+            <form method="post" action="/robot/batch-add" class="form-horizontal" id="searchForm"
                   name="searchForm" enctype="multipart/form-data">
                 <fieldset>
                     <div class="form-group">
                         <div class="col-sm-10">
                             <div class="col-md-3">
-                                <input type="file" class="form-control" id="name" name="name" accept="image/*">
+                                <input type="file" class="form-control" id="name" name="name" accept="*.csv,*.xls,*.xlsx">
                             </div>
                         </div>
                     </div>
@@ -22,11 +21,22 @@ $this->title = '上传文件';
                     </div>
                 </fieldset>
             </form>
+
+            <form action="/robot/download-template" id="downloadForm">
+                <button type="submit" class="mb-sm btn btn-primary ripple" class="download">下载</button>
+            </form>
+
         </div>
     </div>
 </div>
 <script>
     $("#searchBtn").click(function () {
         $("#searchForm").submit()
+    });
+    //导出EXCEL
+    $("#download").click(function () {
+        alert(1);
+        $('#downloadForm').attr('action', '/robot/download-template');
+        $("#downloadForm").submit()
     });
 </script>
