ccaf60f1018012f70017617b47d6185fd72b4126
huangmengrong <huangmengrong@huangmengrong.com>
2018-06-04 15:49:54 +0800

    调整websocket配置参数

--- console/controllers/WebSocketController.php
+++ console/controllers/WebSocketController.php
@@ -24,7 +24,7 @@ class WebSocketController extends Controller
             'max_connection' => Constants::WS_WEB_SOCKET_MAX_CONNECTION, // 最大链接数
             'worker_num' => Constants::WS_WORKER_NUM, // worker 数
             'socket_buffer_size' => intval(Constants::WS_SOCKET_BUFFER_SIZE), // M 必须为数字 用于设置客户端连接最大允许占用内存数量
-            'buffer_output_size' => intval(Constants::WS_BUFFER_OUTPUT_SIZE )// 用于设置单次最大发送长度 M
+//            'buffer_output_size' => intval(Constants::WS_BUFFER_OUTPUT_SIZE )// 用于设置单次最大发送长度 M
         ];
         $server->set($setConfig);
         //添加一个监听端口，继续支持ws方式进行连接
