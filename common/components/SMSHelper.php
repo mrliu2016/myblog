cd449f29a53de1a82017e829cfdf3cac8133b67c
huangmengrong <huangmengrong@huangmengrong.com>
2018-06-11 14:27:37 +0800

    短信验证码

--- common/components/SMSHelper.php
+++ common/components/SMSHelper.php
@@ -66,4 +66,50 @@ class SMSHelper
         }
         return $content;
     }
+
+    /**
+     * 阿里云短信服务器迁移后调用
+     *
+     * @param $service
+     * @param $phone
+     * @param string $templateCode
+     * @return bool|null|\stdClass|string
+     */
+    public static function sendCaptcha($service, $phone, $templateCode = 'SMS_107825044')
+    {
+        $params = array();
+        $accessKeyId = "LTAI7qIs9avI22uS";
+        $accessKeySecret = "Rvp7DHSwwcMIEREFTSrrk3PGXXM9ZS";
+
+        $content = null;
+        $phoneList = explode(',', $phone);
+        foreach ($phoneList as $phone) {
+            $params["PhoneNumbers"] = $phone;
+            $params["SignName"] = "三体云";
+            $params["TemplateCode"] = $templateCode;
+            $params['TemplateParam'] = Array(
+                "service" => $service,
+            );
+            if (!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+                $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+            }
+            $helper = new SignatureHelper();
+            try {
+                // 此处可能会抛出异常，注意catch
+                $content = $helper->request(
+                    $accessKeyId,
+                    $accessKeySecret,
+                    "dysmsapi.aliyuncs.com",
+                    array_merge($params, array(
+                        "RegionId" => "cn-hangzhou",
+                        "Action" => "SendSms",
+                        "Version" => "2017-05-25",
+                    ))
+                );
+            } catch (\Exception $ex) {
+                $content = 'issue';
+            }
+        }
+        return $content;
+    }
 }
