71a924d6d487fa360b0bfee8ee2facd5d8abbb4a
liuhongzhen <liuhongzhen@3ttech.cn>
2018-06-11 19:55:16 +0800

    礼物管理提交页

--- manager/views/gift/index.php
+++ manager/views/gift/index.php
@@ -7,7 +7,7 @@ $this->title = '礼物管理';
     <div class="s-gift">
         <div class="s-gift-search">
             <!--<p class="s-gift-search-title">礼物管理</p>-->
-            <form method="get" action="/gift/template" id="searchForm" name="searchForm">
+            <form method="get" action="/gift/index" id="searchForm" name="searchForm">
                 <div class="s-gift-search-content">
                     <div class="s-gift-search-item">
                         <span>ID</span>
