32dc1fdbeb38b86061022a6e8b13943725938e8a
liuhongzhen <liuhongzhen@3ttech.cn>
2018-06-12 14:39:01 +0800

    优化违禁词批量上传功能

--- manager/views/contraband/batch-word.php
+++ manager/views/contraband/batch-word.php
@@ -23,31 +23,58 @@ $this->title = '上传文件';
                     <button type="submit" class="mb-sm btn btn-primary ripple" id="upload">提交</button>
                 </div>
             </div>
-
-
             <form action="/contraband/download-template" id="downloadForm">
                 <button type="submit" class="mb-sm btn btn-primary ripple" class="download">下载</button>
             </form>
-
         </div>
     </div>
 </div>
+
+<!--确认是否删除start-->
+<div id="confirm_frame" style="display: none">
+    <div class="c-modal-mask"></div>
+    <div class="c-modal-wrap s-banlive">
+        <div class="c-modal">
+           <!-- <div class="c-modal-close s-banlive-close">关闭</div>-->
+            <div class="s-banlive-content">
+                <span class="s-banlive-confirm-text"></span>
+            </div>
+            <div class="c-modal-footer s-banlive-operate">
+                <button class="c-btn c-btn-primary c-btn--large s-banlive-confirm">确认</button>
+                <!--<button class="c-btn c-btn--large s-banlive-cancel">取消</button>-->
+            </div>
+        </div>
+    </div>
+</div>
+<!--确认是否删除end-->
 <script>
-    // $("#searchBtn").click(function () {
-    //     $("#searchForm").submit()
-    // });
     //上传模板
     $("#upload").click(function (event) {
         //判断是否有文件
         var fileEl = $('#File1');
         if (typeof(fileEl[0].files[0])=='undefined'){
             fileEl[0].focus();
-            // html = '<h4 style="color: red">请选择一个文件' + '</h4>';
-            alert('请选择一个模板文件');
+            $("#confirm_frame").css("display","block");
+            $(".s-banlive-confirm-text").text("请选择一个正确的模板文件!");
+            $(".s-banlive-confirm").unbind("click").bind("click",function () {
+                $("#confirm_frame").css("display","none");
+            });
             event.preventDefault();
             return;
         }
-        $("#searchForm").submit();
+
+        var filePath = $('#File1').val().toLowerCase().split(".");
+        var fileType =  filePath[filePath.length - 1];
+        if(fileType == "xls" || fileType == "xlsx"){
+            $("#searchForm").submit();
+        }
+        else{
+            $("#confirm_frame").css("display","block");
+            $(".s-banlive-confirm-text").text("请选择.xls或者.xlsx格式的文件!");
+            $(".s-banlive-confirm").unbind("click").bind("click",function () {
+                $("#confirm_frame").css("display","none");
+            });
+        }
     });
 
     //下载模板
