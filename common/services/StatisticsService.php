05aa66c6bcdf092c0468bb2488a09b6b0a49f6eb
huangmengrong <huangmengrong@huangmengrong.com>
2018-06-27 16:35:25 +0800

    统计直播websocket连接、房间人数

---
+++ common/services/StatisticsService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\common\services;
+
+use app\common\components\RedisClient;
+use Yii;
+
+class StatisticsService
+{
+    /**
+     * 获取 web socket 连接
+     * @return int
+     */
+    public static function getConnectionNum()
+    {
+        $redis = RedisClient::getInstance();
+        return intval($redis->hget(Constants::WS_CONNECTION, Constants::WS_CONNECTION));
+    }
+
+    public static function processLiveRoom($result)
+    {
+        $redis = RedisClient::getInstance();
+        $ip = Yii::$app->params['wsServer']['ip'];
+        foreach ($result as $key => $value) {
+            $keyWSRoomFD = Constants::WS_ROOM_FD . $ip . '_' . $value['roomId'];
+            $num = $redis->hGetAll($keyWSRoomFD);
+            $result[$key]['num'] = !empty($num) ? $num : Constants::CODE_SUCCESS;
+        }
+        return $result;
+    }
+}
