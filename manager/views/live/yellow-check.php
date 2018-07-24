16a886c5b60f1a9b204794ea034bcff00446bcf3
liuhongzhen <liuhongzhen@3ttech.cn>
2018-07-24 19:05:15 +0800

    处理鉴黄报错

--- manager/views/live/yellow-check.php
+++ manager/views/live/yellow-check.php
@@ -10,24 +10,23 @@ $this->title = '鉴黄管理';
         <ul class="s-am_list">
             <li class="s-am_item">
                 <div class="s-am_item-img">
-                    <?php if(!empty($yellowurl)):?>
-                        <img src="<?=$yellowurl?>" width="230px" height="230px"/></div>
-                    <?php else:?>
-                        <img src=""/>
-                    <? endif;?>
-<!--                </div>-->
+                    <?php if (!empty($yellowurl)): ?>
+                    <img src="<?= $yellowurl ?>" width="230px" height="230px"/></div>
+                <?php else: ?>
+                    <img src=""/>
+                <?php endif; ?>
                 <div class="s-am_item-info">
 						<span class="s-am_item-tag">
 							<span>标签：</span>
-							<span><?=$information['Label']?></span>
+							<span><?= $information['Label']?></span>
 						</span>
                     <span class="s-am_item-scene">
 							<span>场景：</span>
-							<span><?=$information['Scene']?></span>
+							<span><?= $information['Scene']?></span>
 						</span>
                 </div>
             </div>
-            </li>
-        </ul>
-    </div>
+        </li>
+    </ul>
+</div>
 </div>
