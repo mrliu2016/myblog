ca1d1203a45e4d2a0d30004db1864df3452794a5
huangmengrong <huangmengrong@huangmengrong.com>
2018-08-21 10:25:54 +0800

    修改获取ws地址方式

--- api/controllers/ServerController.php
+++ api/controllers/ServerController.php
@@ -11,35 +11,37 @@ class ServerController extends BaseController
 {
     public function actionLocation()
     {
-//        $params = Yii::$app->request->get();
-//        $result = ServerResourcesService::getWebSocketServer($params,null, true);
-//        if ($result['code'] == Constants::CODE_SUCCESS) {
-//            ServerResourcesService::roomServerList($params, $result);
-//            ServerResourcesService::setAppIdRoomMap($result, $params);
-//            $this->jsonReturnSuccess(
-//                Constants::CODE_SUCCESS,
-//                '获取成功',
-//                [
-//                    'roomServer' => [
-//                        'ip' => $result['ip'],
-//                        'host' => $result['domain'],
-//                        'port' => $result['wsPort'],
-//                    ],
-//                    'roomServer-wss' => [
-//                        'ip' => $result['ip'],
-//                        'host' => $result['domain'],
-//                        'port' => $result['wssPort'],
-//                    ]
-//                ]
-//            );
-//        }
-//        $this->jsonReturnError(Constants::CODE_SYSTEM_BUSY, '系统繁忙，请稍后重试!');
-
         $params = Yii::$app->request->get();
-        $this->jsonReturnSuccess(
-            Constants::CODE_SUCCESS,
-            '获取成功',
-            LiveService::serverInfo($params)
-        );
+        if (YII_DEBUG) {
+            $this->jsonReturnSuccess(
+                Constants::CODE_SUCCESS,
+                '获取成功',
+                LiveService::serverInfo($params)
+            );
+        } else {
+            $result = ServerResourcesService::getWebSocketServer($params, null, true);
+            if ($result['code'] == Constants::CODE_SUCCESS) {
+                ServerResourcesService::roomServerList($params, $result);
+                ServerResourcesService::setAppIdRoomMap($result, $params);
+                $this->jsonReturnSuccess(
+                    Constants::CODE_SUCCESS,
+                    '获取成功',
+                    [
+                        'roomServer' => [
+                            'ip' => $result['ip'],
+                            'host' => $result['domain'],
+                            'port' => $result['wsPort'],
+                        ],
+                        'roomServer-wss' => [
+                            'ip' => $result['ip'],
+                            'host' => $result['domain'],
+                            'port' => $result['wssPort'],
+                        ]
+                    ]
+                );
+            }
+        }
+        $this->jsonReturnError(Constants::CODE_SYSTEM_BUSY, '系统繁忙，请稍后重试!');
+
     }
 }
